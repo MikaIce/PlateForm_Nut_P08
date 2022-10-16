d2b97e70b65cd6ffe8a67794123aa7818b57a6c4
MikaIce <michael.benoit13@gmail.com>
2022-10-16 16:39:01 -0300

    add Heroku

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -93,10 +93,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'purbeurre',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 if os.environ.get('ENV') == 'PRODUCTION':
