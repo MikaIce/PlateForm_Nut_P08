6f4e89edb120dbc3968e70fef1ef17aa4ae1082f
MikaIce <michael.benoit13@gmail.com>
2023-04-15 03:20:09 +0000

    add travis

--- settings/travis.py
+++ settings/travis.py
@@ -1,17 +1,13 @@
 from purbeurre.settings import *
 
-# Paramètres spécifiques à l'environnement Travis CI
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'travis_ci_test',  # Nom de la base de données pour Travis
-        'USER': 'postgres',  # Utilisateur par défaut de Travis
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',
+        'USER': 'postgres',
         'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
-
-# Autres paramètres spécifiques à Travis CI, si nécessaire
-# Par exemple, vous pouvez modifier le niveau de journalisation ou désactiver certains plugins
 
